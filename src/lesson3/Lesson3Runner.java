2f13f42b4d39b511d3fc75594e2688fec68f3546
Inga <inga_ai@yahoo.com>
2019-12-18 22:39:17 +0200

    1st try, commit

---
+++ src/lesson3/Lesson3Runner.java
@@ -0,0 +1,10 @@
+package lesson3;
+
+public class Lesson3Runner {
+    public static void main(String[] args) {
+        Lesson3 myObject = new Lesson3();
+        //int result = myObject.sum( 5, "www");
+        String defn = myObject.defaultName;
+        //myObject.
+    }
+}
---
+++ src/lesson3/accessModifiers/Runner.java
@@ -0,0 +1,15 @@
+package lesson3.accessModifiers;
+
+import lesson3.Lesson3;
+
+public class Runner {
+    public static void main(String[] args) {
+        Lesson3 myObject = new Lesson3();
+        //int result = myObject.sum( 5, "www");
+        //String defn = myObject.defaultName;
+        //myObject.
+        ExtendedClass xtended = new ExtendedClass();
+
+
+    }
+}
